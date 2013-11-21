d86eab2ad2760091f72bb28b3d3ad96ee33c1df7
Randy <randy.lai@egoal-tech.com>
2013-11-21 16:08:14 +0800

    set up model project
    attachement: sqlsrcipt_V1.0

---
+++ E-BusinessModel/BusinessUser.cs
@@ -0,0 +1,52 @@
+﻿using ORM;
+using ORM.Attributes;
+using System;
+
+namespace E_BusinessModel
+{
+    [DataTable("business_user")]
+    public class BusinessUser : DBObject
+    {
+        private string id;
+        [DataField("id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private DateTime createtime;
+        [DataField("create_datetime")]
+        public DateTime CreateTime
+        {
+            get { return createtime; }
+            set { createtime = value; }
+        }
+
+        private string business_role_id;
+        [DataField("business_role_id")]
+        public string BusinessRoleId
+        {
+            get { return business_role_id; }
+            set { business_role_id = value; }
+        }
+
+        private string user_name;
+        [DataField("business_role_id")]
+        public string UserName
+        {
+            get { return user_name; }
+            set { user_name = value; }
+        }
+
+
+        private string user_password;
+        [DataField("user_password")]
+        public string UserPassWord
+        {
+            get { return user_password; }
+            set { user_password = value; }
+        }
+
+    }
+}
---
+++ E-BusinessModel/CargoKinds.cs
@@ -0,0 +1,42 @@
+﻿using ORM;
+using ORM.Attributes;
+using System;
+
+namespace E_BusinessModel
+{
+    [DataTable("cargo_kinds")]
+    public class CargoKinds : DBObject
+    {
+        private string id;
+        [DataField("id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        [DataField("name")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string type;
+        [DataField("type")]
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        private DateTime createtime;
+        [DataField("createtime")]
+        public DateTime CreateTime
+        {
+            get { return createtime; }
+            set { createtime = value; }
+        }
+
+    }
+}
