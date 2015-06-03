af6fc928386be63f7a14314c4957614d8610570f
RandyGit <361703739@qq.com>
2015-06-03 18:23:40 +0800

    DES

---
+++ WcfService/Common/Utils/Encryptor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Globalization;
+
+namespace CommonHelper
+{
+
+    public class DES
+    {
+        // 创建Key
+        public string GenerateKey()
+        {
+            DESCryptoServiceProvider desCrypto = (DESCryptoServiceProvider)DESCryptoServiceProvider.Create();
+            return ASCIIEncoding.ASCII.GetString(desCrypto.Key);
+        }
+        // 加密字符串
+        public string EncryptString(string sInputString, string sKey)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(sInputString);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+            return BitConverter.ToString(result);
+        }
+        // 解密字符串
+        public string DecryptString(string sInputString, string sKey)
+        {
+            string[] sInput = sInputString.Split("-".ToCharArray());
+            byte[] data = new byte[sInput.Length];
+            for (int i = 0; i < sInput.Length; i++)
+            {
+                data[i] = byte.Parse(sInput[i], NumberStyles.HexNumber);
+            }
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateDecryptor();
+            byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+            return Encoding.UTF8.GetString(result);
+        }
+    }
+
+// DES des = new DES();
+//string key = des.GenerateKey();
+//string s0 = "中国软件 - csdn.net";
+//string s1 = des.EncryptString(s0, key);
+//string s2 = des.DecryptString(s1, key);
+
+    public class Encryptor
+    {
+        public string MD5Encrypt(string pToEncrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.Default.GetBytes(pToEncrypt);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            ret.ToString();
+            return ret.ToString();
+
+
+        }
+
+        public string MD5Decrypt(string pToDecrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            byte[] inputByteArray = new byte[pToDecrypt.Length / 2];
+            for (int x = 0; x < pToDecrypt.Length / 2; x++)
+            {
+                int i = (Convert.ToInt32(pToDecrypt.Substring(x * 2, 2), 16));
+                inputByteArray[x] = (byte)i;
+            }
+
+            des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+
+            StringBuilder ret = new StringBuilder();
+
+            return System.Text.Encoding.Default.GetString(ms.ToArray());
+        }
+
+
+
+    }
+}
